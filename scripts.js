05402bcd37561f2d83df6a7b4814e9e8a1194efc
shulgin01 <shulgin01@inbox.ru>
2022-08-25 17:30:27 +0300

    refresh script

--- scripts.js
+++ scripts.js
@@ -4,46 +4,47 @@
             node.addEventListener(event, handler);
         });
     }
+    function selectTab(node, select, newId) {
+        const newTab = node.querySelector(`.section__tab[data-id=${newId}]`);
+        const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
+        const oldTab = node.querySelector('.section__tab_active');
+        const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
+
+        oldTab.classList.remove('section__tab_active');
+        oldTab.setAttribute('aria-selected', 'false');
+        oldTab.removeAttribute('tabindex');
+        newTab.classList.add('section__tab_active');
+        newTab.setAttribute('aria-selected', 'true');
+        newTab.setAttribute('tabindex', '0');
+        newTab.focus({
+            preventScroll: true
+        });
+
+        oldPanel.classList.add('section__panel_hidden');
+        oldPanel.setAttribute('aria-hidden', 'true');
+        newPanel.classList.remove('section__panel_hidden');
+        newPanel.setAttribute('aria-hidden', 'false');
+
+        select.value = newId
+    }
 
     function makeTabs(node) {
         let selected = node.querySelector('.section__tab_active').dataset.id;
         const tabs = node.querySelectorAll('.section__tab');
         const list = Array.from(tabs).map(node => node.dataset.id);
+
+
         const select = node.querySelector('.section__select');
 
-        function selectTab(newId) {
-            const newTab = node.querySelector(`.section__tab[data-id=${newId}]`);
-            const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
-            const oldTab = node.querySelector('.section__tab_active');
-            const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
-
-            selected = newId;
-
-            oldTab.classList.remove('section__tab_active');
-            oldTab.setAttribute('aria-selected', 'false');
-            oldTab.removeAttribute('tabindex');
-            newTab.classList.add('section__tab_active');
-            newTab.setAttribute('aria-selected', 'true');
-            newTab.setAttribute('tabindex', '0');
-            newTab.focus({
-                preventScroll: true
-            });
-
-            oldPanel.classList.add('section__panel_hidden');
-            oldPanel.setAttribute('aria-hidden', 'true');
-            newPanel.classList.remove('section__panel_hidden');
-            newPanel.setAttribute('aria-hidden', 'false');
-
-            select.value = newId;
-        }
-
         select.addEventListener('input', () => {
-            selectTab(select.value);
+            const newId = select.value
+            selectTab(node, select, newId)
         });
+
 
         bind(tabs, 'click', event => {
             const newId = event.target.dataset.id;
-            selectTab(newId);
+            selectTab(node, select, newId)
         });
 
         bind(tabs, 'keydown', event => {
@@ -74,7 +75,7 @@
                 index = list.length - 1;
             }
 
-            selectTab(list[index]);
+            selectTab(node, select, list[index]);
             event.preventDefault();
         });
     }
@@ -86,14 +87,14 @@
         node.addEventListener('click', () => {
             expanded = !expanded;
             node.setAttribute('aria-expanded', expanded ? 'true' : 'false');
-            node.querySelector('.header__menu-text').textContent = expanded ? 'Закрыть меню' : 'Открыть меню';
+            node.querySelector('.header__menu-text').innerHTML = expanded ? 'Закрыть меню' : 'Открыть меню';
             links.classList.toggle('header__links_opened', expanded);
             links.classList.add('header__links-toggled');
         });
     }
 
     document.addEventListener('DOMContentLoaded', () => {
-        Array.from(document.querySelectorAll('.main__devices')).forEach(makeTabs);
-        Array.from(document.querySelectorAll('.header__menu')).forEach(makeMenu);
+        makeTabs(document.querySelector('.main__devices'))
+        makeMenu(document.querySelector('.header__menu'))
     });
 })();
